865d2bfc57d869d9f5bcf716bb77c0e4e77b4026
ameerhamza@gmail.com <ameerhamza@gmail.com>
2024-01-17 15:53:51 +0400

    final changes

---
+++ TestApp.Server/TestApp.Test/TestBootstrap.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using TestApp.Services.Contracts;
+
+namespace TestApp.Tests
+{
+    public class TestBootstrap
+    {
+        public Mock<IPersonService> MockPersonService { get; private set; }
+
+        public TestBootstrap()
+        {
+            // Initialize mocks for dependencies
+            MockPersonService = new Mock<IPersonService>();
+        }
+    }
+}
